b629fc3c17bfb65ed4c926f597cf84e846669172
KailanySousa <kailany-sousa2010@hotmail.com>
2025-06-12 20:50:23 -0300

    create variants

--- libs/storybook-host/src/stories/button-component.stories.ts
+++ libs/storybook-host/src/stories/button-component.stories.ts
@@ -14,6 +14,7 @@ export const PrimaryButton: Story = {
     text: 'Action',
     variant: 'primary',
     disabled: false,
+    theme: 'blue',
   },
 };
 
@@ -49,6 +50,51 @@ export const TertiaryButton: Story = {
 export const TertiaryButtonDisabled: Story = {
   args: {
     ...PrimaryButton.args,
+    disabled: true,
+  },
+};
+
+export const PrimaryButtonViolet: Story = {
+  args: {
+    text: 'Action',
+    variant: 'primary',
+    disabled: false,
+    theme: 'violet',
+  },
+};
+
+export const PrimaryButtonDisabledViolet: Story = {
+  args: {
+    ...PrimaryButtonViolet.args,
+    disabled: true,
+  },
+};
+
+export const SecondaryButtonViolet: Story = {
+  args: {
+    ...PrimaryButtonViolet.args,
+    variant: 'secondary',
+  },
+};
+
+export const SecondaryButtonDisabledViolet: Story = {
+  args: {
+    ...SecondaryButton.args,
+    disabled: true,
+  },
+};
+
+export const TertiaryButtonViolet: Story = {
+  args: {
+    ...PrimaryButtonViolet.args,
+    variant: 'tertiary',
+    disabled: false,
+  },
+};
+
+export const TertiaryButtonDisabledViolet: Story = {
+  args: {
+    ...TertiaryButtonViolet.args,
     disabled: true,
   },
 };
